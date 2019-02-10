ae42cb03bd545247dcbea43ddafb3c5e77b501f5
Yvette <yvmontes@gmail.com>
2019-02-10 15:18:17 -0800

    added css update js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,40 +1,132 @@
-$(document).ready(function() {
 
-// create an array of words
-var wordsToGuess = [
-    "green day",
-    "good charlotte",
-    "fall out boy",
-    "all american rejects",
-    "all time low",
-    "my chemical romance",
-    "jimmy eat world",
-    "yellowcard",
-    "panic at the disco",
-    "simple plan"
-]
+// array of alternative bands
+var altBands = ["GREENDAY", 
+                "ALLTIMELOW",
+                "YELLOWCARD", 
+                "GOODCHARLOTTE", 
+                "FALLOUTBOY", 
+                "JIMMYEATWORLD", 
+                "PANICATTHEDISCO", 
+                "SIMPLEPLAN", 
+                "WETHEKINGS"];
 
-const maxGuess = 10
-var pauseGame = false
+var totalGuesses = 9;       // number of tries
+var userGuesses = [];       // letters the user guessed
+var computerPick;           // array number the machine choose randomly
+var wordGuessed = [];       // This will be the word we actually build to match the current word
+var guessesLeft = 0;        // How many tries the player has left
+var finishedGame = false;   // Flag for 'press any key to try again'     
+var wins = 0;               //wins
+var losses = 0;             //losses
 
-var guessedLetter = []
-var guessingWord = []
-var wordToMatch
-var numberGuess
-var wins = 0
 
+// game and functions
+function startGame() {
+    guessesLeft = totalGuesses;
+
+    //grab a random number from the alt bands array  (number of words)
+    computerPick = Math.floor(Math.random() * (altBands.length));
+    
+    // Clear out arrays
+    userGuesses = [];
+    wordGuessed = [];
+
+    //build the word with blanks
+    for (var i = 0; i < altBands[computerPick].length; i++) {
+        wordGuessed.push("_");
+    }   
+
+    //gamewin, youlose, title 
+    document.getElementById("pressKeyTryAgain").style.cssText= "display: none";
+    document.getElementById("youlose-image").style.cssText = "display: none";
+    document.getElementById("youwin-image").style.cssText = "display: none";
+
+    //refresh the screen
+    refreshScreen();
+};
+
+//  Updates the display on the HTML Page
+function refreshScreen() {
+    document.getElementById("gameWins").innerText = wins;
+    document.getElementById("gameLosses").innerText = losses;
+
+    var guessingWordText = "";
+    for (var i = 0; i < wordGuessed.length; i++) {
+        guessingWordText += wordGuessed[i];
+    }
+
+    //update guesses, word, and letters entered
+    document.getElementById("currentWord").innerText = guessingWordText;
+    document.getElementById("guessesLeft").innerText = guessesLeft;
+    document.getElementById("userGuesses").innerText = userGuesses;
+};
+
+//compare letters entered to the character you're trying to guess
+function evaluateGuess(letter) {
+    var positions = [];
+
+    for (var i = 0; i < altBands[computerPick].length; i++) {
+        if(altBands[computerPick][i] === letter) {
+            positions.push(i);
+        }
+    }
+
+    if (positions.length <= 0) {
+        guessesLeft--;
+    } else {
+        for(var i = 0; i < positions.length; i++) {
+            wordGuessed[positions[i]] = letter;
+        }
+    }
+};
+
+//check if all letters have been entered.
+function checkWin() {
+    if(wordGuessed.indexOf("_") === -1) {
+        document.getElementById("youwin-image").style.cssText = "display: block";
+        wins++;
+        finishedGame = true;
+    }
+};
+
+//check if the user is out of guesses
+function checkLoss()
+{
+    if(guessesLeft <= 0) {
+        document.getElementById("youlose-image").style.cssText = "display: block";
+        losses++;
+        finishedGame = true;
+    }
 }
 
+//guessing
+function makeGuess(letter) {
+    if (guessesLeft > 0) {
+        // Make sure we didn't use this letter
+        if (userGuesses.indexOf(letter) === -1) {
+            userGuesses.push(letter);
+            evaluateGuess(letter);
+        }
+    }
+};
+
+// Event listener
+document.onkeydown = function(event) {
+    //if the game is finished, restart it.
+    if(finishedGame) {
+        startGame();
+        finishedGame = false;
+    } else {
+        // Check to make sure a-z was pressed.
+        if(event.keyCode >= 65 && event.keyCode <= 90) {
+            makeGuess(event.key.toUpperCase());
+            refreshScreen();
+            checkWin();
+            checkLoss();
+        }
+    }
+};
 
 
 
-// pick a random word
-var word = words[Math.floor(Math.random() * words.length)];
-
-// this empty array lets us match the number of letters in the word
-var answerArray = [];
-for (var i = 0; i < word.length; i++) {
-    answerArray[i] = "_";
-}
-var remainingLetters = word.length;
-
+  
